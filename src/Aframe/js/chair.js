70506f36527326c35443a874d45ec97ad829d026
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-02-27 16:00:36 -0500

    Setup initial frontend repo structure.

---
+++ src/Aframe/js/chair.js
@@ -0,0 +1,31 @@
+AFRAME.registerComponent('chair', {
+    schema: {
+        jsonData: {
+            parse: JSON.parse,
+            stringify: JSON.stringify
+        }
+    },
+    multiple: true,
+    /**
+     * Initial creation and setting of the mesh.
+     */
+    init: function () {
+        var data = this.data.jsonData;
+        var el = this.el;
+
+        // Create geometry.
+        this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+
+        // Create material.
+        this.material = new THREE.MeshStandardMaterial({color: data.color});
+        
+        // Create mesh.
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        
+        el.object3D.position.set(data.x, data.y, data.z)
+        // Set mesh on entity.
+        el.setObject3D('mesh', this.mesh);
+        console.log("here");
+    }
+});
+
