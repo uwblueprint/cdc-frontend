590cde90216b1437238f4e383edae69eebf6f67f
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-03-17 20:58:26 -0400

    Restructure src folder.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./React/index.css";
-import App from "./React/App";
-import reportWebVitals from "./React/reportWebVitals";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
