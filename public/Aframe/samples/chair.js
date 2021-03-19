01c92c41b21ab382da6115f1d17c3fce930b5a07
amoliksingh <amoliksingh@hotmail.com>
2021-03-18 21:04:24 -0400

    Test Prettier for linting html files

--- public/Aframe/samples/chair.js
+++ public/Aframe/samples/chair.js
@@ -1,9 +1,9 @@
-AFRAME.registerComponent('chair', {
+AFRAME.registerComponent("chair", {
     schema: {
         jsonData: {
             parse: JSON.parse,
-            stringify: JSON.stringify
-        }
+            stringify: JSON.stringify,
+        },
     },
     multiple: true,
     /**
@@ -14,17 +14,20 @@ AFRAME.registerComponent('chair', {
         const el = this.el;
 
         // Create geometry.
-        this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+        this.geometry = new THREE.BoxBufferGeometry(
+            data.width,
+            data.height,
+            data.depth
+        );
 
         // Create material.
-        this.material = new THREE.MeshStandardMaterial({color: data.color});
-        
+        this.material = new THREE.MeshStandardMaterial({ color: data.color });
+
         // Create mesh.
         this.mesh = new THREE.Mesh(this.geometry, this.material);
-        
-        el.object3D.position.set(data.x, data.y, data.z)
-        // Set mesh on entity.
-        el.setObject3D('mesh', this.mesh);
-    }
-});
 
+        el.object3D.position.set(data.x, data.y, data.z);
+        // Set mesh on entity.
+        el.setObject3D("mesh", this.mesh);
+    },
+});
--- public/Aframe/samples/script.js
+++ public/Aframe/samples/script.js
@@ -4,14 +4,14 @@ let container = document.querySelector("#container");
 let colors = ["red", "orange", "yellow", "green", "blue", "purple", "hotpink"];
 
 for (let i = 0; i < 75; i++) {
-  addBall();
+    addBall();
 }
 
 function addBall() {
-  let x = Math.random() * 10 - 5;
-  let y = Math.random() * 50 + 2;
-  let z = Math.random() * -10;
-  container.innerHTML += `<a-sphere click-drag dynamic-body position="${x} ${y} ${z}" radius="0.5" color="${
-    colors[Math.floor(Math.random() * colors.length)]
-  }" mass="0.5"></a-sphere>`;
+    let x = Math.random() * 10 - 5;
+    let y = Math.random() * 50 + 2;
+    let z = Math.random() * -10;
+    container.innerHTML += `<a-sphere click-drag dynamic-body position="${x} ${y} ${z}" radius="0.5" color="${
+        colors[Math.floor(Math.random() * colors.length)]
+    }" mass="0.5"></a-sphere>`;
 }
