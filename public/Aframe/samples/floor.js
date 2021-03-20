58128dfc742f4daa0144bec510c3f4b10186920f
amoliksingh <amoliksingh@hotmail.com>
2021-03-20 17:53:24 -0400

    Fix lint errors

--- public/Aframe/samples/floor.js
+++ public/Aframe/samples/floor.js
@@ -1,22 +1,20 @@
-AFRAME.registerComponent('floor', {
-  multiple: true,
-    init: function(){
-      
-    }
-  });
-  
-AFRAME.registerComponent('camera-floor', {
-  dependencies: ['floor'],
-  init: function(){
-    renderFloor();
-}
+AFRAME.registerComponent("floor", {
+    multiple: true,
+    init: function () {},
 });
 
-async function renderFloor(){
-  setTimeout(function() {
-    var playerEl = document.querySelector('[camera]');
-    playerEl.setAttribute('look-controls', 'pointerLockEnabled: true');
-    playerEl.setAttribute('wasd-controls', '');
-    playerEl.setAttribute('kinematic-body', '');
-  }, (4 * 1000));
+AFRAME.registerComponent("camera-floor", {
+    dependencies: ["floor"],
+    init: function () {
+        renderFloor();
+    },
+});
+
+async function renderFloor() {
+    setTimeout(function () {
+        var playerEl = document.querySelector("[camera]");
+        playerEl.setAttribute("look-controls", "pointerLockEnabled: true");
+        playerEl.setAttribute("wasd-controls", "");
+        playerEl.setAttribute("kinematic-body", "");
+    }, 4 * 1000);
 }
