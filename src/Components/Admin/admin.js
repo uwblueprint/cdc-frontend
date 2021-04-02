829dc32607020b1cb77af19b3777ee955d73bef3
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-04-01 21:27:47 -0400

    Basic page navigation.

--- src/Components/Admin/admin.js
+++ src/Components/Admin/admin.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
@@ -6,6 +7,45 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { auth } from "../../firebaseCredentials.js";
 import { UserContext } from "../../Providers/UserProviders";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import EscapeRooms from "./rooms.js";
+import Assets from "./assets.js";
+import Statistics from "./stats.js";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`wrapped-tabpanel-${index}`}
+            aria-labelledby={`wrapped-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function TabHelper(index) {
+    return {
+        id: `wrapped-tab-${index}`,
+        "aria-controls": `wrapped-tabpanel-${index}`,
+    };
+}
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -17,11 +57,21 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
 }));
 
 export default function Admin() {
     const classes = useStyles();
     const user = useContext(UserContext);
+
+    const [value, setValue] = React.useState("rooms");
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     return (
         <Container component="main" maxWidth="xs">
@@ -45,6 +95,38 @@ export default function Admin() {
                 >
                     Sign Out
                 </Button>
+                <div className={classes.root}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        aria-label="wrapped label tabs example"
+                    >
+                        <Tab
+                            value="rooms"
+                            label="Escape Rooms"
+                            {...TabHelper("rooms")}
+                        />
+                        <Tab
+                            value="assets"
+                            label="Object Assets"
+                            {...TabHelper("assets")}
+                        />
+                        <Tab
+                            value="stats"
+                            label="Statistics"
+                            {...TabHelper("stats")}
+                        />
+                    </Tabs>
+                    <TabPanel value={value} index="rooms">
+                        <EscapeRooms />
+                    </TabPanel>
+                    <TabPanel value={value} index="assets">
+                        <Assets />
+                    </TabPanel>
+                    <TabPanel value={value} index="stats">
+                        <Statistics />
+                    </TabPanel>
+                </div>
             </div>
         </Container>
     );
