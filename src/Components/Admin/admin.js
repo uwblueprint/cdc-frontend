7e4373d1762c1b38ee78574df9f856158fbadecd
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-04-09 00:16:57 -0400

    Finalize designs for tabs. Restructure admin structure.

--- src/Components/Admin/admin.js
+++ src/Components/Admin/admin.js
@@ -10,9 +10,11 @@ import { UserContext } from "../../Providers/UserProviders";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
-import EscapeRooms from "./rooms.js";
-import Assets from "./assets.js";
-import Statistics from "./stats.js";
+import EscapeRooms from "./Rooms/rooms.js";
+import Assets from "./Assets/assets.js";
+import Statistics from "./Stats/stats.js";
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -59,7 +61,40 @@ const useStyles = makeStyles((theme) => ({
     },
     root: {
         flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
+        background: "#fafafa",
+    },
+    tab: {
+        maxWidth: "182px",
+        height: "65px",
+        background: "#E4EBFF",
+        borderTopRightRadius: "32px",
+        borderTopLeftRadius: "32px",
+        top: "20px",
+        fontFamily: "Arial",
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: "18px",
+        lineHeight: "20px",
+        color: "#737272",
+        textTransform: "capitalize",
+        marginBottom: "10px",
+    },
+    addButton: {
+        marginTop: theme.spacing(3),
+        marginRight: theme.spacing(1),
+        width: "50px",
+        height: "50px",
+        background: "#B9BECE",
+        borderRadius: "24.5px",
+        color: "white",
+        float: "right",
+    },
+    tabBackground: {
+        background: "#E4EBFF",
+        width: "1000px",
+        borderBottomRightRadius: "32px",
+        borderBottomLeftRadius: "32px",
+        borderTopRightRadius: "32px",
     },
 }));
 
@@ -96,34 +131,55 @@ export default function Admin() {
                     Sign Out
                 </Button>
                 <div className={classes.root}>
+                    <IconButton
+                        className={classes.addButton}
+                        aria-label="delete"
+                    >
+                        <AddIcon />
+                    </IconButton>
                     <Tabs
                         value={value}
+                        indicatorColor="primary"
                         onChange={handleChange}
-                        aria-label="wrapped label tabs example"
                     >
                         <Tab
+                            className={classes.tab}
                             value="rooms"
                             label="Escape Rooms"
                             {...TabHelper("rooms")}
                         />
                         <Tab
+                            className={classes.tab}
                             value="assets"
                             label="Object Assets"
                             {...TabHelper("assets")}
                         />
                         <Tab
+                            className={classes.tab}
                             value="stats"
                             label="Statistics"
                             {...TabHelper("stats")}
                         />
                     </Tabs>
-                    <TabPanel value={value} index="rooms">
+                    <TabPanel
+                        className={classes.tabBackground}
+                        value={value}
+                        index="rooms"
+                    >
                         <EscapeRooms />
                     </TabPanel>
-                    <TabPanel value={value} index="assets">
+                    <TabPanel
+                        className={classes.tabBackground}
+                        value={value}
+                        index="assets"
+                    >
                         <Assets />
                     </TabPanel>
-                    <TabPanel value={value} index="stats">
+                    <TabPanel
+                        className={classes.tabBackground}
+                        value={value}
+                        index="stats"
+                    >
                         <Statistics />
                     </TabPanel>
                 </div>
