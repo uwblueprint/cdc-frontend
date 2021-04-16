b877b1fb3d4e7eae8829f387b4c99ab2c10232a3
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2021-04-15 21:18:04 -0400

    Use admin backend url

--- src/lib/dataAccess.js
+++ src/lib/dataAccess.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BACKEND_URL + "/";
+const baseURL = process.env.REACT_APP_ADMIN_BACKEND_URL + "/";
 
 export const httpGet = (endPoint, accessToken) => {
     const url = baseURL + endPoint;
