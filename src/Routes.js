947974c51abcc30420fc636fde1d6f37552fd102
Aaron Abraham <aaronabraham311@gmail.com>
2021-04-18 17:09:14 -0600

    Scene setup and top bars (#29)
    * Initial scene setup
    
    * Reworking navbar and stacked app bar
    
    * linting
    
    * Cleanup

--- src/Routes.js
+++ src/Routes.js
@@ -5,26 +5,26 @@ import Signup from "./Components/Login/signup";
 import Home from "./Components/Home/home";
 import Admin from "./Components/Admin/admin";
 import PasswordReset from "./Components/Login/passwordReset";
+import EnvironmentEditor from "./Components/Admin/EnvironmentEditor/environment";
 import { UserContext } from "./Providers/UserProviders";
 
 export default function Routes() {
     const user = useContext(UserContext);
     return user ? (
-        <Admin />
+        <Switch>
+            <Route exact path="/" component={Admin} />
+            <Route
+                exact
+                path="/environment/:environmentId"
+                component={EnvironmentEditor}
+            />
+        </Switch>
     ) : (
         <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route exact path="/login">
-                <Login />
-            </Route>
-            <Route exact path="/signup">
-                <Signup />
-            </Route>
-            <Route exact path="/passwordReset">
-                <PasswordReset />
-            </Route>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/passwordReset" component={PasswordReset} />
         </Switch>
     );
 }
