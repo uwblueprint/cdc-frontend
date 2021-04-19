39b3b6e511428a0d90810433940fbdc6578f1e9c
Aaron Abraham <aaronabraham311@gmail.com>
2021-04-18 19:04:35 -0600

    Empty scene setup (#30)
    * Initial scene setup
    
    * Reworking navbar and stacked app bar
    
    * linting
    
    * Cleanup
    
    * Adding empty scene buttons

--- src/Components/Admin/EnvironmentEditor/environment.js
+++ src/Components/Admin/EnvironmentEditor/environment.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import AddIcon from "@material-ui/icons/Add";
 
 import Navbar from "../navbar";
 import EnvironmentBar from "./environmentBar";
@@ -7,6 +10,31 @@ import EnvironmentBar from "./environmentBar";
 const useStyles = makeStyles((theme) => ({
     page: {
         marginTop: theme.spacing(8),
+    },
+    container: {
+        paddingTop: theme.spacing(12),
+    },
+    emptyButtonsContainer: {
+        display: "flex",
+        width: "100%",
+        flexDirection: "column",
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "space-around",
+    },
+    buttonContainer: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        marginTop: theme.spacing(12),
+        marginBottom: theme.spacing(4),
+    },
+    button: {
+        display: "flex",
+        justifyContent: "center",
+        padding: "16px 12px",
+        backgroundColor: "#E2E5ED",
+        borderRadius: "12px",
     },
 }));
 
@@ -18,6 +46,26 @@ export default function EnvironmentEditor() {
             <Navbar home />
             <div className={classes.page}>
                 <EnvironmentBar />
+            </div>
+            <div className={classes.container}>
+                <div className={classes.emptyButtonsContainer}>
+                    <div className={classes.buttonContainer}>
+                        <Button
+                            startIcon={<AddIcon />}
+                            className={classes.button}
+                        >
+                            New Scene from Scratch
+                        </Button>
+                    </div>
+                    <div className={classes.buttonContainer}>
+                        <Button
+                            startIcon={<AccountBalanceIcon />}
+                            className={classes.button}
+                        >
+                            New Scene from Template
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
