622c89b883ee9e10557b54010ea88a34c8ce5296
Aaron Abraham <aaronabraham311@gmail.com>
2021-05-03 09:49:26 -0600

    Create scene from dropdown

--- src/lib/sceneEndpoints.js
+++ src/lib/sceneEndpoints.js
@@ -1,8 +1,17 @@
-import { httpGet } from "./dataAccess";
+import { httpGet, httpPost } from "./dataAccess";
 
 const baseEndpoint = process.env.REACT_APP_ADMIN_BASE_ENDPOINT;
 
 export const getScene = async (id) => {
     const response = await httpGet(baseEndpoint + `scene/${id}`);
+    return response.data;
+};
+
+export const createScene = async (name, background_id) => {
+    const response = await httpPost(baseEndpoint + `scene`, {
+        name,
+        background_id,
+    });
+
     return response.data;
 };
