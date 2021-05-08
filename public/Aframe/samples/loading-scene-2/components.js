06e736e58efdd98f7e0c186c24b505a47e91a395
Kevin Hu <kevin.hu@uwaterloo.ca>
2021-05-08 14:01:56 -0400

    removed console logs

--- public/Aframe/samples/loading-scene-2/components.js
+++ public/Aframe/samples/loading-scene-2/components.js
@@ -1,7 +1,6 @@
 AFRAME.registerComponent("collision-detect", {
     init: function () {
         this.el.addEventListener("collide", function (e) {
-            console.log("Player has collided with ", e.detail.body.el);
             e.detail.target.el; // Original entity (playerEl).
             e.detail.body.el; // Other entity, which playerEl touched.
             e.detail.contact; // Stats about the collision (CANNON.ContactEquation).
