24f1478b7a36b4914be8d71353a6eded9f70b24d
Kevin Hu <kevin.hu@uwaterloo.ca>
2021-06-08 22:03:06 -0400

    initial work for background id scene dropdown

--- src/Components/Admin/EnvironmentEditor/sceneModal.js
+++ src/Components/Admin/EnvironmentEditor/sceneModal.js
@@ -7,6 +7,9 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { getAllAssets } from "../../../lib/assetEndpoints";
+import { useErrorHandler } from "react-error-boundary";
 
 const useStyles = makeStyles(() => ({
     textField: {
@@ -26,6 +29,7 @@ export default function SceneModal({
     isEdit,
 }) {
     const classes = useStyles();
+    const handleError = useErrorHandler();
     const [sceneName, setSceneName] = React.useState("");
     const [backgroundId, setBackgroundId] = React.useState(null);
     const [description, setDescription] = React.useState("");
@@ -34,8 +38,17 @@ export default function SceneModal({
         backgroundId: "",
         description: "",
     });
+    const [assets, setAssets] = React.useState([]);
 
     useEffect(() => {
+        const getAsset = async () => {
+            const data = await getAllAssets(handleError);
+            const modifiedData = data.filter(
+                (asset) => asset.obj_type === "background"
+            );
+            setAssets(modifiedData);
+        };
+
         if (scene) {
             setSceneName(scene ? scene.name : "");
             setBackgroundId(scene ? scene.background_id : null);
@@ -50,34 +63,33 @@ export default function SceneModal({
                       }
             );
         }
-    }, [scene]);
+        getAsset();
+    }, [scene, handleError]);
 
     const handleSceneNameChange = (event) => {
         setSceneName(event.target.value);
-        setErrors({ name: "" });
+        setErrors({ ...errors, name: "" });
         const reg = new RegExp(/^[a-zA-Z0-9 _-]{1,50}$/).test(
             event.target.value
         );
         if (!reg) {
             setErrors({
+                ...errors,
                 name:
                     "Only characters allowed are alphanumeric (a-z, A-Z, 0-9), dashes (- and _), and spaces",
             });
         }
     };
 
-    const handleSceneBackgroundChange = (event) => {
-        setBackgroundId(event.target.value);
-    };
-
     const handleSceneDescriptionChange = (event) => {
         setDescription(event.target.value);
-        setErrors({ description: "" });
+        setErrors({ ...errors, description: "" });
         const reg = new RegExp(/^[?!.,a-zA-Z0-9 _-]{1,2000}$/).test(
             event.target.value
         );
         if (!reg) {
             setErrors({
+                ...errors,
                 description:
                     "Only characters allowed are alphanumeric (a-z, A-Z, 0-9), dashes (- and _), punctuation (?!.,), and spaces",
             });
@@ -90,13 +102,26 @@ export default function SceneModal({
                 ? errors.name || errors.backgroundId || errors.description
                 : false
         );
+
         if (!error) {
             handleSubmit(sceneName, parseInt(backgroundId), description);
         }
     };
 
+    const handleModalCloseClick = () => {
+        setSceneName("");
+        setDescription("");
+        setBackgroundId(null);
+        setErrors({
+            name: "",
+            backgroundId: "",
+            description: "",
+        });
+        handleModalClose();
+    };
+
     return (
-        <Dialog open={modalOpen} onClose={handleModalClose}>
+        <Dialog open={modalOpen} onClose={handleModalCloseClick}>
             <DialogTitle>{isEdit ? "Edit Scene" : "New Scene"}</DialogTitle>
             <DialogContent>
                 <div>
@@ -107,17 +132,35 @@ export default function SceneModal({
                         className={classes.textField}
                         required
                         error={Boolean(errors ? errors.name : false)}
-                        helperText={errors ? errors.name : false}
+                        helperText={errors ? errors.name : ""}
                     />
                 </div>
-                <div>
-                    <Typography>Scene background ID: </Typography>
-                    <TextField
-                        value={backgroundId}
-                        onChange={handleSceneBackgroundChange}
-                        className={classes.textField}
-                    />
-                </div>
+                {!isEdit && (
+                    <div>
+                        <Typography>Scene background ID: </Typography>
+                        <Autocomplete
+                            id="backgroundIdSelection"
+                            style={{ width: 300 }}
+                            options={assets}
+                            autoHighlight
+                            getOptionLabel={(option) => option.name}
+                            onChange={(_event, value) =>
+                                setBackgroundId(value ? value.id : null)
+                            }
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Choose a background"
+                                    variant="outlined"
+                                    inputProps={{
+                                        ...params.inputProps,
+                                        autoComplete: "new-password",
+                                    }}
+                                />
+                            )}
+                        />
+                    </div>
+                )}
                 <div>
                     <Typography>Description: </Typography>
                     <TextField
@@ -126,13 +169,16 @@ export default function SceneModal({
                         className={classes.textField}
                         required
                         error={Boolean(errors ? errors.description : false)}
-                        helperText={errors ? errors.description : false}
+                        helperText={errors ? errors.description : ""}
                     />
                 </div>
             </DialogContent>
             <DialogActions className={classes.buttonContainer}>
                 <Button onClick={handleModalClose}> Cancel </Button>
-                <Button onClick={handleModalSubmitClick}>
+                <Button
+                    onClick={handleModalSubmitClick}
+                    disabled={!sceneName || !description || !backgroundId}
+                >
                     {isEdit ? "Edit" : "Create"}
                 </Button>
             </DialogActions>
