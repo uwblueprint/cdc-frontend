889a5a75401e49468ad66dc33db30c658ba80d83
Kevin Hu <kevin.hu@uwaterloo.ca>
2021-07-03 16:00:12 -0400

    Added a MUI themes folder and used it to modify App.js (#91)
    * initial commit
    
    * tried to fix npm issues
    
    * Reset package-lock.json file
    
    * fixed colour scheme and added extra fonts
    
    Co-authored-by: Ahmed Hamodi <ahmed.r.hamodi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import UserProvider from "./Providers/UserProviders.jsx";
 import Navbar from "./Components/Admin/navbar.js";
 import ErrorModal from "./Components/Admin/common/errorModal.js";
 import { ErrorBoundary } from "react-error-boundary";
+import "./styles/index.css";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Theme } from "./styles/Theme";
 
 function App() {
     return (
         <ErrorBoundary FallbackComponent={ErrorModal}>
-            <UserProvider>
-                <div className="App container py-3">
-                    <Navbar />
-                    <Routes />
-                </div>
-            </UserProvider>
+            <ThemeProvider theme={Theme}>
+                <UserProvider>
+                    <div className="App container py-3">
+                        <Navbar />
+                        <Routes />
+                    </div>
+                </UserProvider>
+            </ThemeProvider>
         </ErrorBoundary>
     );
 }
