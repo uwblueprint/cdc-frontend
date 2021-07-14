e836397429ba4db347928433423f3b90c7821350
amoliksingh <amoliksingh@hotmail.com>
2021-07-13 21:12:54 -0400

    Actually fix the delayed picture issue

--- src/Components/Admin/ObjectEditor/unorderedpuzzle.js
+++ src/Components/Admin/ObjectEditor/unorderedpuzzle.js
@@ -38,7 +38,6 @@ export default function UnorderedPuzzle(props) {
     const [images, setImages] = React.useState(props.images);
     const [curIndex, setCurIndex] = React.useState(0);
     const [imagesLen, setImagesLen] = React.useState(0);
-    const [curImageSrc, setCurImageSrc] = React.useState(null);
 
     const imagesLengthList = [
         { value: 2, label: "2" },
@@ -72,13 +71,12 @@ export default function UnorderedPuzzle(props) {
             const imagePrefix = process.env.REACT_APP_ADMIN_ASSET_PREFIX;
             const imagesCopy = images;
             imagesCopy[curIndex].imageSrc = imagePrefix + response.data.s3_key;
-            setCurImageSrc(imagePrefix + response.data.s3_key);
             setImages(imagesCopy);
             props.saveImageN(curIndex, imagePrefix + response.data.s3_key);
         };
 
         const uploadImage = async () => {
-            handleUploadImageSubmit(name, type, "image", imageByteArray);
+            await handleUploadImageSubmit(name, type, "image", imageByteArray);
             setUploaded(true);
             setImageByteArray(null);
         };
@@ -162,11 +160,6 @@ export default function UnorderedPuzzle(props) {
                                     max-width={1000}
                                     object-fit={"contain"}
                                 ></img>
-                            </div>
-                        ) : null}
-                        {index === curIndex ? (
-                            <div style={{ visibility: "hidden" }}>
-                                {curImageSrc}
                             </div>
                         ) : null}
                         <input
