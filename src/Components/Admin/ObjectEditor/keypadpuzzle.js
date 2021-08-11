41ef6f266b78ee27c47bd550d5aee008ce1e68de
amoliksingh <amoliksingh@hotmail.com>
2021-08-10 21:44:15 -0400

    Add TextField logic

--- src/Components/Admin/ObjectEditor/keypadpuzzle.js
+++ src/Components/Admin/ObjectEditor/keypadpuzzle.js
@@ -1,28 +1,73 @@
 import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
-import { IconButton } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import { DeleteForever } from "@material-ui/icons";
 import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+    input: {
+        "&:invalid": {
+            border: "red solid 2px",
+        },
+    },
+}));
 
 export default function KeypadPuzzle(props) {
+    const classes = useStyles();
+    const [tempPass, setTempPass] = useState(props.pass);
     const [pass, setPass] = useState(props.pass);
+    const [showNumpadPrompt, setNumpadPrompt] = useState(false);
+    const [showKeyboardPrompt, setKeyboardPrompt] = useState(false);
+    const [showSavePass, setSavePass] = useState(false);
 
-    const addPass = () => {
-        const newPass = prompt("Enter the text for the puzzle: ");
-
-        if (newPass) {
-            if (newPass !== /^[0-9.,]+$/) {
-                alert("Error: Password may only contain numbers");
-            } else {
-                props.savePass(newPass);
-                setPass(newPass);
-            }
+    const showPrompt = () => {
+        if (props.isNumpad) {
+            setNumpadPrompt(true);
+        } else {
+            setKeyboardPrompt(true);
         }
+        setSavePass(true);
     };
 
     const deletePass = () => {
         props.savePass("");
         setPass("");
+        if (props.isNumpad) {
+            setNumpadPrompt(false);
+        } else {
+            setKeyboardPrompt(false);
+        }
+        setSavePass(false);
+    };
+
+    const savePass = () => {
+        props.savePass(tempPass);
+        setPass(tempPass);
+    };
+
+    const numpadOnChange = (e) => {
+        const re = /^[0-9\b]{1,7}$/;
+        if (e.target.value === "" || re.test(e.target.value)) {
+            setTempPass(e.target.value);
+        } else {
+            alert(
+                "Error: Password must be at most 7 characters and only consist of numbers"
+            );
+            setSavePass(false);
+        }
+    };
+
+    const keyboardOnChange = (e) => {
+        const re = /^[0-9a-zA-Z\b]{1,7}$/;
+        if (e.target.value === "" || re.test(e.target.value)) {
+            setTempPass(e.target.value);
+        } else {
+            alert(
+                "Error: Password must be at most 7 characters and must be alphanumeric"
+            );
+            setSavePass(false);
+        }
     };
 
     return (
@@ -33,19 +78,32 @@ export default function KeypadPuzzle(props) {
                     <IconButton
                         className={props.classes.addButton}
                         aria-label="add"
-                        onClick={addPass}
+                        onClick={showPrompt}
                     >
                         <AddIcon />
                     </IconButton>
-                    {props.isNumpad ? (
+                    {props.isNumpad && showNumpadPrompt ? (
                         <TextField
-                            id="standard-number"
-                            label="Number"
-                            type="number"
-                            InputLabelProps={{
-                                shrink: true,
+                            inputProps={{
+                                className: classes.input,
+                                pattern: "[0-9]{1,7}",
                             }}
+                            onChange={numpadOnChange}
                         />
+                    ) : null}
+                    {!props.isNumpad && showKeyboardPrompt ? (
+                        <TextField
+                            inputProps={{
+                                className: classes.input,
+                                pattern: "[0-9a-zA-Z]{1,7}",
+                            }}
+                            onChange={keyboardOnChange}
+                        />
+                    ) : null}
+                    {showSavePass ? (
+                        <Button color="primary" onClick={() => savePass()}>
+                            Save Password
+                        </Button>
                     ) : null}
                 </div>
             ) : (
