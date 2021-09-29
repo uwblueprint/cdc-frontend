f517a4b4e1e9c495e820faba40ef0b5eca44561a
Amolik Singh <amoliksingh@hotmail.com>
2021-09-28 22:46:12 -0400

    Puzzle CRUD: Keypad Puzzles (#106)
    * Initial work for keypad puzzles
    
    * Working version for keypad puzzles
    
    * Rework alerts and start adding TextFields
    
    * Small state changes
    
    * Add TextField logic
    
    * Fix errors
    
    * Fix regex and use saved password
    
    * Remove unneeded alert
    
    Co-authored-by: Dhruvin Balar <35475783+drbalar@users.noreply.github.com>

---
+++ src/Components/Admin/ObjectEditor/keypadpuzzle.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import { Button, IconButton } from "@material-ui/core";
+import { DeleteForever } from "@material-ui/icons";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+    input: {
+        "&:invalid": {
+            border: "red solid 2px",
+        },
+    },
+}));
+
+export default function KeypadPuzzle(props) {
+    const classes = useStyles();
+    const [tempPass, setTempPass] = useState(props.pass);
+    const [pass, setPass] = useState(props.pass);
+    const [showNumpadPrompt, setNumpadPrompt] = useState(false);
+    const [showKeyboardPrompt, setKeyboardPrompt] = useState(false);
+    const [showSavePass, setSavePass] = useState(false);
+
+    const showPrompt = () => {
+        if (props.isNumpad) {
+            setNumpadPrompt(true);
+        } else {
+            setKeyboardPrompt(true);
+        }
+        setSavePass(true);
+    };
+
+    const deletePass = () => {
+        props.savePass("");
+        setPass("");
+        if (props.isNumpad) {
+            setNumpadPrompt(false);
+        } else {
+            setKeyboardPrompt(false);
+        }
+        setSavePass(false);
+    };
+
+    const savePass = () => {
+        props.savePass(tempPass);
+        setPass(tempPass);
+    };
+
+    const numpadOnChange = (e) => {
+        const re = /^[0-9]{1,7}$/;
+        if (e.target.value === "" || re.test(e.target.value)) {
+            setTempPass(e.target.value);
+            setSavePass(true);
+        } else {
+            alert(
+                "Error: Password must be at most 7 characters and only consist of numbers"
+            );
+            setSavePass(false);
+        }
+    };
+
+    const keyboardOnChange = (e) => {
+        const re = /^[0-9a-zA-Z]{1,16}$/;
+        if (e.target.value === "" || re.test(e.target.value)) {
+            setTempPass(e.target.value);
+            setSavePass(true);
+        } else {
+            alert(
+                "Error: Password must be at most 16 characters and must be alphanumeric"
+            );
+            setSavePass(false);
+        }
+    };
+
+    return (
+        <div>
+            {pass === "" ? (
+                <div>
+                    Add Password
+                    <IconButton
+                        className={props.classes.addButton}
+                        aria-label="add"
+                        onClick={showPrompt}
+                    >
+                        <AddIcon />
+                    </IconButton>
+                    {props.isNumpad && showNumpadPrompt ? (
+                        <TextField
+                            inputProps={{
+                                className: classes.input,
+                                pattern: "[0-9]{1,7}",
+                            }}
+                            onChange={numpadOnChange}
+                        />
+                    ) : null}
+                    {!props.isNumpad && showKeyboardPrompt ? (
+                        <TextField
+                            inputProps={{
+                                className: classes.input,
+                                pattern: "[0-9a-zA-Z]{1,16}",
+                            }}
+                            onChange={keyboardOnChange}
+                        />
+                    ) : null}
+                    {showSavePass ? (
+                        <Button color="primary" onClick={() => savePass()}>
+                            Save Password
+                        </Button>
+                    ) : null}
+                </div>
+            ) : (
+                <div>
+                    Password: {pass}
+                    <IconButton onClick={() => deletePass()}>
+                        <DeleteForever />
+                    </IconButton>
+                </div>
+            )}
+        </div>
+    );
+}
