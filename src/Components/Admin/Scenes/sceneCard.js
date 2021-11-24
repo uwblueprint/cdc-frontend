636e231b88a238983b7b2a9bf17ea96db1f38db4
Dhruvin Balar <dhruvinb123@gmail.com>
2021-11-23 21:36:34 -0500

    Update scene page

--- src/Components/Admin/Scenes/sceneCard.js
+++ src/Components/Admin/Scenes/sceneCard.js
@@ -6,6 +6,8 @@ import IconButton from "@material-ui/core/IconButton";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
+import { Colours } from "../../../styles/Constants.ts";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 const useStyles = makeStyles(() => ({
     card: {
@@ -13,21 +15,66 @@ const useStyles = makeStyles(() => ({
     },
     cardImage: {
         width: "90%",
-        height: "200px",
+        height: "180px",
         maxWidth: 400,
         marginTop: 16,
         objectFit: "cover",
+        borderTopLeftRadius: "4px",
+        borderTopRightRadius: "4px",
+    },
+    selectedCardImage: {
+        width: "90%",
+        height: "180px",
+        maxWidth: 400,
+        marginTop: 16,
+        objectFit: "cover",
+        borderTopLeftRadius: "4px",
+        borderTopRightRadius: "4px",
+        borderLeft: "solid",
+        borderRight: "solid",
+        borderTop: "solid",
+        borderLeftWidth: "2px",
+        borderRightWidth: "2px",
+        borderTopWidth: "2px",
+        borderLeftColor: Colours.MainRed5,
+        borderRightColor: Colours.MainRed5,
+        borderTopColor: Colours.MainRed5,
     },
     metadata: {
         display: "flex",
         justifyContent: "space-between",
-        marginLeft: "5.7%",
-        marginRight: "5.7%",
+        fontSize: 16,
+        marginLeft: "6%",
+        marginRight: "6%",
         marginTop: -4,
         backgroundColor: "white",
-        borderBottomLeftRadius: "12px",
-        borderBottomRightRadius: "12px",
-        height: 60,
+        height: 80,
+        borderBottomLeftRadius: "4px",
+        borderBottomRightRadius: "4px",
+        border: "solid",
+        borderWidth: "1px",
+        borderColor: "#DEE2E6",
+    },
+    selectedMetadata: {
+        display: "flex",
+        justifyContent: "space-between",
+        fontSize: 16,
+        marginLeft: "6%",
+        marginRight: "6%",
+        marginTop: -4,
+        backgroundColor: "white",
+        height: 80,
+        borderBottomLeftRadius: "4px",
+        borderBottomRightRadius: "4px",
+        borderLeft: "solid",
+        borderRight: "solid",
+        borderBottom: "solid",
+        borderLeftWidth: "2px",
+        borderRightWidth: "2px",
+        borderBottomWidth: "2px",
+        borderLeftColor: Colours.MainRed5,
+        borderRightColor: Colours.MainRed5,
+        borderBottomColor: Colours.MainRed5,
     },
     dataName: {
         lineHeight: "normal",
@@ -46,6 +93,7 @@ export default function SceneCard({
 }) {
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [isHighlighted, setIsHighlighted] = React.useState(false);
 
     const handleMenuClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -56,6 +104,15 @@ export default function SceneCard({
     };
 
     const handleCardClick = () => {
+        setIsHighlighted(true);
+        console.log(isHighlighted);
+    };
+
+    const handleClickAway = () => {
+        setIsHighlighted(false);
+    };
+
+    const handleDoubleClick = () => {
         window.open(
             process.env.REACT_APP_ADMIN_BACKEND_URL + "/admin/scene/" + data.id
         );
@@ -64,91 +121,102 @@ export default function SceneCard({
     const open = Boolean(anchorEl);
 
     return (
-        <Grid
-            container
-            item
-            xs={12}
-            md={6}
-            lg={4}
-            spacing={1}
-            alignItems="center"
-            justify="flex-start"
-            className={classes.card}
-        >
+        <ClickAwayListener onClickAway={handleClickAway}>
             <Grid
                 container
                 item
                 xs={12}
+                md={6}
+                lg={4}
+                spacing={1}
                 alignItems="center"
-                justify="center"
+                justify="flex-start"
+                className={classes.card}
                 onClick={handleCardClick}
+                onDoubleClick={handleDoubleClick}
             >
-                <img
-                    className={classes.cardImage}
-                    src={
-                        data.screenshot_url
-                            ? data.screenshot_url
-                            : defaultSceneImage
+                <Grid
+                    container
+                    item
+                    xs={12}
+                    alignItems="center"
+                    justify="center"
+                >
+                    <img
+                        className={
+                            isHighlighted
+                                ? classes.selectedCardImage
+                                : classes.cardImage
+                        }
+                        src={
+                            data.screenshot_url
+                                ? data.screenshot_url
+                                : defaultSceneImage
+                        }
+                        alt="Scene"
+                    />
+                </Grid>
+                <Grid
+                    container
+                    item
+                    xs={12}
+                    alignItems="center"
+                    className={
+                        isHighlighted
+                            ? classes.selectedMetadata
+                            : classes.metadata
                     }
-                    alt="Scene"
-                />
+                >
+                    <p className={classes.dataName}>{data.name}</p>
+                    <IconButton
+                        onClick={handleMenuClick}
+                        style={{ maxWidth: "20%" }}
+                    >
+                        <MoreVertIcon />
+                    </IconButton>
+                </Grid>
+                <Menu
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: "top",
+                        horizontal: "center",
+                    }}
+                    transformOrigin={{
+                        vertical: "top",
+                        horizontal: "center",
+                    }}
+                    keepMounted
+                    open={open}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem
+                        onClick={() => {
+                            setAnchorEl(null);
+                            handleDoubleClick();
+                        }}
+                    >
+                        Open Inspector
+                    </MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            setAnchorEl(null);
+                            handleEditClick(data.id);
+                        }}
+                    >
+                        Edit scene metadata
+                    </MenuItem>
+                    <MenuItem>Copy game link</MenuItem>
+                    <MenuItem>View stats</MenuItem>
+                    <MenuItem
+                        onClick={() => {
+                            setAnchorEl(null);
+                            handleDeleteClick(data.id);
+                        }}
+                    >
+                        Delete scene
+                    </MenuItem>
+                </Menu>
             </Grid>
-            <Grid
-                container
-                item
-                xs={12}
-                alignItems="center"
-                className={classes.metadata}
-            >
-                <p className={classes.dataName}>{data.name}</p>
-                <IconButton
-                    onClick={handleMenuClick}
-                    style={{ maxWidth: "20%" }}
-                >
-                    <MoreVertIcon />
-                </IconButton>
-            </Grid>
-            <Menu
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "center",
-                }}
-                transformOrigin={{
-                    vertical: "top",
-                    horizontal: "center",
-                }}
-                keepMounted
-                open={open}
-                onClose={handleMenuClose}
-            >
-                <MenuItem
-                    onClick={() => {
-                        setAnchorEl(null);
-                        handleCardClick();
-                    }}
-                >
-                    Open Inspector
-                </MenuItem>
-                <MenuItem
-                    onClick={() => {
-                        setAnchorEl(null);
-                        handleEditClick(data.id);
-                    }}
-                >
-                    Edit scene metadata
-                </MenuItem>
-                <MenuItem>Copy game link</MenuItem>
-                <MenuItem>View stats</MenuItem>
-                <MenuItem
-                    onClick={() => {
-                        setAnchorEl(null);
-                        handleDeleteClick(data.id);
-                    }}
-                >
-                    Delete scene
-                </MenuItem>
-            </Menu>
-        </Grid>
+        </ClickAwayListener>
     );
 }
