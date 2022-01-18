384dfe6e09cb991bcde1a79f8b292c4773400037
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2022-01-18 13:58:45 -0500

    Make fields edittable

--- src/Components/Admin/EnvironmentEditor/conclusionModal.js
+++ src/Components/Admin/EnvironmentEditor/conclusionModal.js
@@ -3,19 +3,15 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import Dialog from "@material-ui/core/Dialog";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import _ from "lodash";
 
-const useStyles = makeStyles((theme) => ({
-    addButton: {
-        marginTop: theme.spacing(0.5),
-        width: "20px",
-        height: "20px",
-        background: "#B9BECE",
-        borderRadius: "100%",
-        color: "white",
-        float: "right",
+const useStyles = makeStyles(() => ({
+    textField: {
+        width: "500px",
     },
 }));
 
@@ -33,13 +29,97 @@ export default function ConclusionModal({
     const [shareLink, setShareLink] = useState(conclusionData.share_link);
     const originalConclusionData = _.cloneDeep(conclusionData);
 
+    const handleHeaderTextChange = (event) => {
+        const response = event.target.value;
+        setHeaderText(response);
+    };
+
+    const handleParagraphTextChange = (event) => {
+        const response = event.target.value;
+        setParagraphText(response);
+    };
+
+    const handleShareLinkChange = (event) => {
+        const response = event.target.value;
+        setShareLink(response);
+    };
+
     return (
         <Dialog open={modalOpen} onClose={handleModalClose}>
             <DialogTitle>Modify Conclusion Data</DialogTitle>
             <DialogContent>
-                <p>{headerText}</p>
-                <p>{paragraphText}</p>
-                <p>{shareLink}</p>
+                <div style={{ paddingBottom: 15 }}>
+                    <Typography
+                        style={{
+                            fontSize: 20,
+                            lineHeight: "27px",
+                        }}
+                    >
+                        Header Text
+                    </Typography>
+                    <TextField
+                        value={headerText}
+                        className={classes.textField}
+                        onChange={handleHeaderTextChange}
+                        required
+                        variant="outlined"
+                        multiline
+                        inputProps={{
+                            style: {
+                                padding: "0px 10px",
+                            },
+                        }}
+                        placeholder="header-text"
+                    />
+                </div>
+                <div style={{ paddingBottom: 15 }}>
+                    <Typography
+                        style={{
+                            fontSize: 20,
+                            lineHeight: "27px",
+                        }}
+                    >
+                        Paragraph Text
+                    </Typography>
+                    <TextField
+                        value={paragraphText}
+                        className={classes.textField}
+                        onChange={handleParagraphTextChange}
+                        required
+                        variant="outlined"
+                        multiline
+                        inputProps={{
+                            style: {
+                                padding: "0px 10px",
+                            },
+                        }}
+                        placeholder="header-text"
+                    />
+                </div>
+                <div style={{ paddingBottom: 15 }}>
+                    <Typography
+                        style={{
+                            fontSize: 20,
+                            lineHeight: "27px",
+                        }}
+                    >
+                        Share Link
+                    </Typography>
+                    <TextField
+                        value={shareLink}
+                        className={classes.textField}
+                        onChange={handleShareLinkChange}
+                        required
+                        variant="outlined"
+                        multiline
+                        inputProps={{
+                            style: {
+                                padding: "0px 10px",
+                            },
+                        }}
+                        placeholder="header-text"
+                    />
+                </div>
             </DialogContent>
             <DialogActions>
                 <Button
