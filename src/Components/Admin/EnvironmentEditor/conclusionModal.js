d980913b948b9d857977df3354dadfd41cad645d
Ahmed Hamodi <ahmed.r.hamodi@gmail.com>
2022-01-20 11:45:58 -0500

    Conclusion page customizability (#135)
    * Initial work on conclusion page customizability
    
    * Make editScenario call on conclusion modal submit
    
    * Make fields edittable
    
    * Final touches

---
+++ src/Components/Admin/EnvironmentEditor/conclusionModal.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import _ from "lodash";
+
+const useStyles = makeStyles(() => ({
+    textField: {
+        width: "500px",
+    },
+}));
+
+export default function ConclusionModal({
+    conclusionData,
+    modalOpen,
+    handleModalClose,
+    handleSubmit,
+}) {
+    const classes = useStyles();
+    const [headerText, setHeaderText] = useState(conclusionData.header_text);
+    const [paragraphText, setParagraphText] = useState(
+        conclusionData.paragraph_text
+    );
+    const [shareLink, setShareLink] = useState(conclusionData.share_link);
+    const originalConclusionData = _.cloneDeep(conclusionData);
+
+    useEffect(() => {
+        setHeaderText(conclusionData.header_text);
+        setParagraphText(conclusionData.paragraph_text);
+        setShareLink(conclusionData.share_link);
+    }, [conclusionData]);
+
+    const handleHeaderTextChange = (event) => {
+        const response = event.target.value;
+        setHeaderText(response);
+    };
+
+    const handleParagraphTextChange = (event) => {
+        const response = event.target.value;
+        setParagraphText(response);
+    };
+
+    const handleShareLinkChange = (event) => {
+        const response = event.target.value;
+        setShareLink(response);
+    };
+
+    return (
+        <Dialog open={modalOpen} onClose={handleModalClose}>
+            <DialogTitle>Modify Conclusion Data</DialogTitle>
+            <DialogContent>
+                <div style={{ paddingBottom: 15 }}>
+                    <Typography
+                        style={{
+                            fontSize: 20,
+                            lineHeight: "27px",
+                        }}
+                    >
+                        Header Text
+                    </Typography>
+                    <TextField
+                        value={headerText}
+                        className={classes.textField}
+                        onChange={handleHeaderTextChange}
+                        required
+                        variant="outlined"
+                        multiline
+                        inputProps={{
+                            style: {
+                                padding: "0px 10px",
+                            },
+                        }}
+                        placeholder="The header text a student will see when finishing the escape room."
+                    />
+                </div>
+                <div style={{ paddingBottom: 15 }}>
+                    <Typography
+                        style={{
+                            fontSize: 20,
+                            lineHeight: "27px",
+                        }}
+                    >
+                        Paragraph Text
+                    </Typography>
+                    <TextField
+                        value={paragraphText}
+                        className={classes.textField}
+                        onChange={handleParagraphTextChange}
+                        required
+                        variant="outlined"
+                        multiline
+                        inputProps={{
+                            style: {
+                                padding: "0px 10px",
+                            },
+                        }}
+                        placeholder="The paragraph text a student will see when finishing the escape room."
+                    />
+                </div>
+                <div style={{ paddingBottom: 15 }}>
+                    <Typography
+                        style={{
+                            fontSize: 20,
+                            lineHeight: "27px",
+                        }}
+                    >
+                        Share Link
+                    </Typography>
+                    <TextField
+                        value={shareLink}
+                        className={classes.textField}
+                        onChange={handleShareLinkChange}
+                        required
+                        variant="outlined"
+                        multiline
+                        inputProps={{
+                            style: {
+                                padding: "0px 10px",
+                            },
+                        }}
+                        placeholder="The link students will see when finishing the escape room."
+                    />
+                </div>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={() => {
+                        setHeaderText(originalConclusionData.header_text);
+                        setParagraphText(originalConclusionData.paragraph_text);
+                        setShareLink(originalConclusionData.share_link);
+                        handleModalClose();
+                    }}
+                >
+                    Cancel
+                </Button>
+                <Button
+                    color="primary"
+                    onClick={() =>
+                        handleSubmit(headerText, paragraphText, shareLink)
+                    }
+                >
+                    Save
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
